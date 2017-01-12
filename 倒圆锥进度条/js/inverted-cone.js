f7673fc82b8f84bca85ce10a61643921d0d2b601
JeannieMa <jeanniema91@gmail.com>
2017-01-12 11:24:47 +0800

    Update inverted-cone.js

--- 倒圆锥进度条/js/inverted-cone.js
+++ 倒圆锥进度条/js/inverted-cone.js
@@ -1,5 +1,6 @@
+
 $(function (){
-  // 圆锥
+  // 圆锥：以累计椭圆轨迹堆积成圆锥
   var x,    //底面圆心xxx
       y,    //底面圆心y
       R,    //椭圆R
@@ -9,38 +10,27 @@ $(function (){
       h=2*R-y;  // 所填变量只要满足h1=2R
   var canvas = $("#myCanvas"),
       ctx = canvas.get(0).getContext('2d');
-      moving(ctx,200,150,180,20,a,210);
-      drawcone(ctx,200,150,180,20);
+      moving(ctx,200,50,180,20,a,500);
+      drawcone(ctx,200,50,180,20,500);
 
       // 正三角形
   var canvas = $("#theCanvas"),
       content = canvas.get(0).getContext('2d'),
       height =200*Math.sin(Math.PI/3),   // //计算边为l的正三角形的高
       y = '';
-      //drawRect(content,height);
       growing(content,height,y);
 });
-function drawcone(ctx,x,y,R,r){
+function drawcone(ctx,x,y,R,r,h){
   $(document).ready(function(){
-  //   if (CanvasRenderingContext2D.prototype.ellipse == undefined) {
-  //   CanvasRenderingContext2D.prototype.ellipse = function(x, y, radiusX, radiusY, rotation, startAngle, endAngle, antiClockwise) {
-  //     this.save();
-  //     this.translate(x, y);
-  //     this.rotate(rotation);
-  //     this.scale(radiusX, radiusY);
-  //     this.arc(0, 0, 1, startAngle, endAngle, antiClockwise);
-  //     this.restore();
-  //   }
-  // }
       // 圆椎外形
       ctx.beginPath();
       ctx.ellipse(x, y, R, r, 0, 0, 2*Math.PI, true);
       ctx.closePath();
       ctx.stroke();
-      ctx.moveTo(r,y);
-      ctx.lineTo(x,2*R);
-      ctx.moveTo(r+2*R,y);
-      ctx.lineTo(x,2*R);
+      ctx.moveTo(x-R,y);
+      ctx.lineTo(x,h+y);
+      ctx.moveTo(x+R,y);
+      ctx.lineTo(x,h+y);
       ctx.stroke();
 
   });
@@ -49,31 +39,39 @@ function drawcone(ctx,x,y,R,r){
 function moving(ctx,x,y,R,r,a,h){
   $('input[name="persentage"]').on('change',function(){
       a=$(this).val();
-      ctx.clearRect(r,y-r,2*R,2*R);  // 清除之前的圆锥
-      drawcone(ctx,x,y,R,r);    // 重画外形
-      for(var i=0;i<=a*1000;i++){
-          ctx.fillStyle="rgb(255,192,75)";
+      var b=parseInt(a*1000);
+      ctx.clearRect(0,0,2*x,h+R);  // 清除之前的圆锥
+      //drawcone(ctx,x,y,R,r);    // 重画外形
+      var i=0;
+      if(i<=b){
+      var t = setInterval(function (){
+          ctx.fillStyle="rgb(245,192,75)";    // bodyColor
           ctx.beginPath();
-          ctx.ellipse(x, 2*R-(i/1000)*h, (i/1000)*(R-1), (i/1000)*(r-1), 0, 0, 2*Math.PI, true);     // a％的椭圆
+          ctx.ellipse(x, h+r-(i/1000)*h, (i/1000)*R, (i/1000)*r, 0, 0, 2*Math.PI, true);     // a％的椭圆
           ctx.closePath();
           ctx.fill();
+          if(i==b){
+            ctx.fillStyle="rgb(245,155,37)";
+            ctx.beginPath();
+            ctx.ellipse(x, h+r-a*h, a*R, a*r, 0, 0, 2*Math.PI, true);
+            ctx.closePath();
+            ctx.stroke();
+            ctx.fill();
+          }
+          ++i;
+
+          if(i>b){
+            clearInterval(t);
+          }
+        },5);
       }
 
-        ctx.strokeType="rgb(100,100,100)";
-        ctx.fillStyle="rgb(245,155,37)";
-        ctx.beginPath();
-        ctx.ellipse(x, 2*R-a*h, a*R, a*r, 0, 0, 2*Math.PI, true);
-        ctx.closePath();
-        ctx.stroke();
-        ctx.fill();
-
-        if(a>0.82){
-          ctx.strokeType="rgb(0,0,0)";
-          ctx.beginPath();
-          ctx.ellipse(x, y, R, r, 0, 0, 2*Math.PI, true); // 圆锥地面仍显示外面框
-          ctx.closePath();
-          ctx.stroke();
-        }
+        // if(a>0.82){
+        //   ctx.beginPath();
+        //   ctx.ellipse(x, y, R, r, 0, 0, 2*Math.PI, true); // 圆锥地面仍显示外面框
+        //   ctx.closePath();
+        //   ctx.stroke();
+        // }
     });
 
 }
@@ -92,10 +90,6 @@ function drawRect(content,height,y){
     content.moveTo(200,height);
     content.lineTo(100,y);
     content.lineTo(100,height);
-    // var grd = content.createLinearGradient(0,0,200,0);//使用渐变颜色填充,从(0,0)到(200,0) （左到右）
-    // grd.addColorStop(0,"#4CE8B2"); //起始颜色
-    // grd.addColorStop(1,"#EFD458"); //终点颜色
-    // content.fillStyle=grd; //以上面定义的渐变填充
     content.closePath();
     content.fill(); //闭合形状并且以填充方式绘制出来
   });
@@ -104,8 +98,19 @@ function drawRect(content,height,y){
 function growing(content,height,y){
   $('input[name="persentageRect"]').on('change',function(){
     var b = $(this).val();
-    y = height - b*height;
-    content.clearRect(0,0,200,200);
-    drawRect(content,height,y);
+    var i=0;
+    if(i<=b*100){
+      var t = setInterval(function (){
+        y = height - (i/100)*height;
+        console.log(height);
+        console.log(y);
+        content.clearRect(0,0,200,200);
+        drawRect(content,height,y);
+        ++i;
+        if(i>b*100){
+          clearInterval(t);
+        }
+      },80);
+    }
   });
 }
